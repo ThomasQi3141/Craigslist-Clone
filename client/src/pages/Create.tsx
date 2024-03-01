f5234b2a2e04dc8aaf3491703835103e8e523def
Thomas Qi <thomaszhaojieqi@gmail.com>
2024-03-01 13:51:49 -0500

    Added Input Fields

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [val, setVal] = useState("");
+  const handleChange = (event: any) => {
+    setVal(event.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="School Name"
+        onChange={handleChange}
+        value={val}
+        className="schoolNameInput"
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        className="descriptionInput"
+      />
+      <button>Bruh</button>
+    </div>
+  );
 };
 
 export default Create;
