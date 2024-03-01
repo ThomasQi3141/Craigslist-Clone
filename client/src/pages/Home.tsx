25ed281337ba81b1561203efa623e8997514e9f8
Thomas Qi <thomaszhaojieqi@gmail.com>
2024-03-01 16:14:59 -0500

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { storage } from "../firebase";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
 
 const Home = () => {
   //Navigate through webpages on click
@@ -6,9 +9,29 @@ const Home = () => {
   const toCreate = () => {
     navigate("/create");
   };
+
+  const imageListRef = ref(storage, "images/");
+
+  const [nameList, setNameList] = useState([]);
+  const [descriptionList, setDescriptionList] = useState([]);
+  const [imageList, setImageList] = useState<string[]>([]);
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList([...imageList, url]);
+        });
+      });
+    });
+  }, []);
+
   return (
     <div>
       <button onClick={toCreate}>Create</button>
+      {imageList.map((url) => {
+        return <img src={url} />;
+      })}
     </div>
   );
 };
