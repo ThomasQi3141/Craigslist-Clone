5269807ece82cbc72a05a9e7d3d9786b96e4950f
Thomas Qi <thomaszhaojieqi@gmail.com>
2024-03-01 15:30:38 -0500

    Added Image Uploading
    Created image uploading feature

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,38 +1,68 @@
 import { useState } from "react";
 import { storage } from "../firebase";
+import { ref, uploadBytes, uploadString } from "firebase/storage";
+import { v4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-  const [val, setVal] = useState("");
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const [imageUpload, setImageUpload] = useState<File | null>(null);
-  const handleChange = (event: any) => {
-    setVal(event.target.value);
+
+  const handleNameChange = (event: any) => {
+    setName(event.target.value);
   };
+
+  const handleDescriptionChange = (event: any) => {
+    setDescription(event.target.value);
+  };
+
+  const handleImageChange = (event: any) => {
+    setImageUpload(event.target.files![0]);
+  };
+
   const submit = () => {
-    if (imageUpload == null) {
-      //Didn't select images
+    if (name === "") {
+      alert("Missing Name");
       return;
     }
+    if (description === "") {
+      alert("Missing Description");
+      return;
+    }
+    if (imageUpload === null) {
+      // No image selected
+      alert("No Image Selected");
+      return;
+    }
+
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload);
+    const nameRef = ref(storage, `names/${name + v4()}`);
+    uploadString(nameRef, name);
+    const descriptionRef = ref(storage, `descriptions/${description + v4()}`);
+    uploadString(descriptionRef, description);
+    alert("Submission Successful");
+    navigate("/");
   };
   return (
     <div>
       <input
         type="text"
         placeholder="School Name"
-        onChange={handleChange}
-        value={val}
+        onChange={handleNameChange}
+        value={name}
         className="schoolNameInput"
       />
       <input
         type="text"
         placeholder="Description"
+        onChange={handleDescriptionChange}
+        value={description}
         className="descriptionInput"
       />
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files![0]);
-        }}
-      />
+      <input type="file" onChange={handleImageChange} />
       <button onClick={submit}>Submit</button>
     </div>
   );
